69de7be8dad62ae6d1572e2c502be9f435cb4a7a
taiki yamashita <taiki0808ty@gmail.com>
2021-02-07 07:09:25 +0900

    誕生日とニックネームの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,12 +25,14 @@ class User < ApplicationRecord
   has_many :boards, dependent: :destroy
   has_one :profile, dependent: :destroy
 
+  delegate :birthday, :gender, to: :profile, allow_nil: true
+
   def has_written?(board)
     boards.exists?(id: board.id)
   end
 
   def display_name
-    self.email.split('@').first
+    profile&.nickname || self.email.split('@').first
   end
 
   def prepare_profile
