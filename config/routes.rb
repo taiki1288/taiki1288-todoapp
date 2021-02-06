1f6f188271ef73cbfd1304fd2f5509e1bcdd42c4
taiki yamashita <taiki0808ty@gmail.com>
2021-02-07 06:38:15 +0900

    プロフィールページと編集ページを実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   root to: 'boards#index'
 
   resources :boards, only: [:show, :new, :create, :edit, :update, :destroy]
+
+  resource :profile, only: [:show, :edit, :update]
 end
