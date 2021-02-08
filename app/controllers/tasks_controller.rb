92c67b5ccfc39151f8ed12c8cdaaaea5896c0153
taiki yamashita <taiki0808ty@gmail.com>
2021-02-08 21:29:06 +0900

    taskを保存、表示させたい

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,6 +2,7 @@ class TasksController < ApplicationController
     before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
     def show
+
     end
 
     def new
@@ -9,4 +10,18 @@ class TasksController < ApplicationController
         @task = board.tasks.build
     end
 
+    def create
+        board = Board.find(params[:board_id])
+        @task = board.tasks.build(task_params)
+        if @task.save
+          redirect_to boards_path(@board)
+        else
+          render :new
+        end
+    end
+
+    private
+    def task_params
+      params.require(:task).permit(:title, :content, :deadline)
+    end
 end
