e6289cb1056ccfd97061fcd9f8b7e808ae371998
taiki yamashita <taiki0808ty@gmail.com>
2021-02-15 00:36:14 +0900

    コメントを表示

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   root to: 'boards#index'
 
   resources :boards do
-    resources :tasks
+    resources :tasks do
+      resources :comments, only: [:new, :create]
+    end
   end
 
   resource :profile, only: [:show, :edit, :update]
